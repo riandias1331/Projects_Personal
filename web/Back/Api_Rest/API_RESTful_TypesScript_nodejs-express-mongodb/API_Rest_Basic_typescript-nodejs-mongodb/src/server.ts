09ecddea9e447dbcd4cdf306b450b710bf5a7347
riandias1331 <pereirarian@gmail.com>
2025-04-21 18:04:21 -0300

    projects

--- web/Back/Api_Rest/API_RESTful_TypesScript_nodejs-express-mongodb/API_Rest_Basic_typescript-nodejs-mongodb/src/server.ts
+++ web/Back/Api_Rest/API_RESTful_TypesScript_nodejs-express-mongodb/API_Rest_Basic_typescript-nodejs-mongodb/src/server.ts
@@ -1,38 +1,18 @@
-// import express, { Application } from 'express';
-// import mongoose from 'mongoose';
-// import router from './routes/userRoutes';
-
-// const app: Application = express();
-// const PORT = 5000;
-
-// // Middleware para JSON
-// app.use(express.json());
-
-// // Conexão com o MongoDB
-// mongoose.connect('mongodb://localhost:27017/restful_api')
-//   .then(() => console.log('Conectado ao MongoDB'))
-//   .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
-
-// // Rotas
-// app.use('/api', router);
-
-// // Iniciar o servidor
-// app.listen(PORT, () => {
-//     console.log(`Servidor rodando na porta ${PORT}`);
-// });
-
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
 import userRoutes from './routes/userRoutes';
 
+const PORT = process.env.PORT || 3333;
 dotenv.config();
 connectDB();
 
 const app = express();
 
 app.use(express.json());
-app.use('/api/users', userRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use(userRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
