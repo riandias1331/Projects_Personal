08838aa757c30892eb210c5aa314ad83cf03a5ee
Rian Dias <pereiradiasrian.@gmail.com>
2025-07-20 21:38:52 -0300

    projects

---
+++ web/Back/create_API/Api_Rest/API_RESTful_TypesScript_nodejs-express-mongodb/API_Rest_typescript-nodejs-mongodb-jwt-prisma/src/controllers/userController.ts
@@ -0,0 +1,9 @@
+import express, { Request, response } from 'express'
+
+// export const getUsers = async(req: Request, res: Response) => {
+//     try {
+//         const user = Number
+//     } catch (error) {
+//         res.status(500).json({ message (error as Error) })
+//     }
+// }
