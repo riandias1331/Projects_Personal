c3864e7770351f6b78d7d2d6e0a6a7438c6791f7
ian Dias <pereiradiasrian@gmail.com>
2025-08-03 17:03:25 -0300

    projects

--- web/Back/create_API/Api_Rest/API_RESTful_TypesScript_nodejs-express-mongodb/API_Rest_typescript-nodejs-mongodb-jwt/routes.ts
+++ web/Back/create_API/Api_Rest/API_RESTful_TypesScript_nodejs-express-mongodb/API_Rest_typescript-nodejs-mongodb-jwt/routes.ts
@@ -5,14 +5,14 @@ const route: Router = Router()
 import * as userController from './src/controllers/userController'
 import * as auth from './src/middlewares/auth'
 
-// Public Routes
+// Private Routes
 route.get("/users", auth.authMiddleware, userController.getUserAll)
 route.get("/users/:id", auth.authMiddleware, userController.getUser)
 route.put("/users/:id", auth.authMiddleware, userController.updateeUser)
 route.delete("/users", auth.authMiddleware, userController.deleteUserAll)
 route.delete("/users/:id", auth.authMiddleware, userController.deleteUser)
 
-// Private Routes
+// Public Routes
 route.post("/users", userController.create)
 
 
